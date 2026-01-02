85e9020a0e33dcb8bc8d4892629e88bd1bc95974
lpv_vql <tom.saitou005137@gmail.com>
2026-01-02 15:41:01 +0900

    Update account.js

--- js/account.js
+++ js/account.js
@@ -1,53 +1,73 @@
 const accountsBySite = {
   "Scratch": [
-    { name: "lpv_vql", link: "https://scratch.mit.edu/users/lpv_vql/", icon: "https://cdn2.scratch.mit.edu/get_image/user/139189676_60x60.png" },
-    { name: "Ougonhi_Hakuginhi", link: "https://scratch.mit.edu/users/Ougonhi_Hakuginhi/", icon: "https://cdn2.scratch.mit.edu/get_image/user/141962855_60x60.png" },
-    { name: "sub_pv", link: "https://scratch.mit.edu/users/sub_pv/", icon: "https://cdn2.scratch.mit.edu/get_image/user/142051866_60x60.png" }
+    { name: "lpv_vql", link: "https://scratch.mit.edu/users/lpv_vql/" },
+    { name: "Ougonhi_Hakuginhi", link: "https://scratch.mit.edu/users/Ougonhi_Hakuginhi/" },
+    { name: "sub_pv", link: "https://scratch.mit.edu/users/sub_pv/" }
   ],
-  "Japanese Scratch-Wiki": [
-    { name: "Lpv vql", link: "https://ja.scratch-wiki.info/wiki/利用者:Lpv_vql", icon: "https://cdn2.scratch.mit.edu/get_image/user/139189676_60x60.png" }
+  "Japanese-Scratch Wiki": [
+    { name: "Lpv vql", link: "https://ja.scratch-wiki.info/wiki/利用者:Lpv_vql" }
   ],
   "YouTube": [
-    { name: "lpv_vql", link: "https://www.youtube.com/@lpv_vql", icon: "https://www.youtube.com/s/desktop/f4f4cfe4/img/favicon_32.png" }
+    { name: "lpv_vql", link: "https://www.youtube.com/@lpv_vql" }
   ],
   "GitHub": [
-    { name: "lpv-vql", link: "https://github.com/lpv-vql", icon: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" }
+    { name: "lpv-vql", link: "https://github.com/lpv-vql" }
   ]
+};
+
+// サイトごとのファビコン
+const siteFavicons = {
+  "Scratch": "https://scratch.mit.edu/favicon.ico",
+  "Japanese-Scratch Wiki": "https://ja.scratch-wiki.info/favicon.ico",
+  "YouTube": "https://www.youtube.com/s/desktop/f4f4cfe4/img/favicon_32.png",
+  "GitHub": "https://github.githubassets.com/favicons/favicon.svg"
 };
 
 const accountContainer = document.getElementById("account-list");
 
 if (accountContainer) {
-  // サイトごとに分けて表示
   Object.keys(accountsBySite).forEach(site => {
-    // サイト名見出し
+    // サイト名 + ファビコン
+    const siteDiv = document.createElement("div");
+    siteDiv.style.marginBottom = "15px";
+    siteDiv.style.display = "flex";
+    siteDiv.style.alignItems = "center";
+    siteDiv.style.gap = "8px";
+
+    const favicon = document.createElement("img");
+    favicon.src = siteFavicons[site];
+    favicon.alt = site + " icon";
+    favicon.style.width = "20px";
+    favicon.style.height = "20px";
+
     const siteTitle = document.createElement("h3");
     siteTitle.textContent = site;
-    siteTitle.style.margin = "10px 0 5px 0";
-    accountContainer.appendChild(siteTitle);
+    siteTitle.style.margin = "0";
 
-    // サイトのアカウントリスト
+    siteDiv.appendChild(favicon);
+    siteDiv.appendChild(siteTitle);
+    accountContainer.appendChild(siteDiv);
+
+    // サイト内アカウント一覧（縦表示）
     const siteList = document.createElement("div");
-    siteList.className = "account-list";
+    siteList.style.marginLeft = "28px"; // ファビコン分少しずらす
     accountsBySite[site].forEach(acc => {
-      const div = document.createElement("div");
-      div.className = "account-item";
-
-      const img = document.createElement("img");
-      img.src = acc.icon;
-      img.alt = acc.name;
-
       const a = document.createElement("a");
       a.href = acc.link;
       a.target = "_blank";
       a.textContent = acc.name;
+      a.style.display = "block";
+      a.style.marginBottom = "4px";
+      a.style.color = "black";
+      a.style.textDecoration = "none";
+      a.style.fontWeight = "bold";
 
-      div.appendChild(img);
-      div.appendChild(a);
-      siteList.appendChild(div);
+      a.onmouseover = () => a.style.textDecoration = "underline";
+      a.onmouseout = () => a.style.textDecoration = "none";
+
+      siteList.appendChild(a);
     });
 
     accountContainer.appendChild(siteList);
   });
 }
-
