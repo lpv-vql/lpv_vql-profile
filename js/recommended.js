20e2c2e149cdc6fd1367dfd6fe45932c5460148b
lpv_vql <tom.saitou005137@gmail.com>
2026-01-02 13:13:03 +0900

    Update recommended.js

--- js/recommended.js
+++ js/recommended.js
@@ -1,11 +1,13 @@
-// おすすめ作品リスト（作品IDまたはURL）
+// おすすめ作品リスト
 const recommendedProjects = [
-  "749818769", // 例: Scratch作品ID
-  "749818770",
-  "749818771"
+  "1223537762",
+  "1204816483",
+  "1172866199",
+  "1071074675",
+  "1052198414"
 ];
 
-// サムネイル取得関数
+// サムネイル取得
 function createProjectThumbnail(projectId) {
   const url = `https://cdn2.scratch.mit.edu/get_image/project/${projectId}_480x360.png`;
   const link = `https://scratch.mit.edu/projects/${projectId}/`;
