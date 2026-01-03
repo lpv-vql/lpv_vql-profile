9729199a7eea383ec80db6340580a40ff05ddd83
lpv_vql <tom.saitou005137@gmail.com>
2026-01-04 07:03:57 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,17 +1,17 @@
+<script>
 document.addEventListener("DOMContentLoaded", () => {
 
-  fetch("/header.html")
+  fetch("../header.html")
     .then(r => r.text())
     .then(html => {
-      const header = document.getElementById("header");
-      if (header) header.innerHTML = html;
+      document.getElementById("header").innerHTML = html;
     });
 
-  fetch("/footer.html")
+  fetch("../footer.html")
     .then(r => r.text())
     .then(html => {
-      const footer = document.getElementById("footer");
-      if (footer) footer.innerHTML = html;
+      document.getElementById("footer").innerHTML = html;
     });
 
 });
+</script>
